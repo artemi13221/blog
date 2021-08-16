fb5636d15f7c7e82f804fe0290a7b58ca5f4ae5d
artemi <artemi13221@gmail.com>
2021-08-16 16:10:54 +0900

    🔨 Add Cookie&Session and login router

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,13 @@ import { ObjectId } from 'mongodb';
 // const { json: bodyParserJson } = require('body-parser');
 // require('dotenv').config();
 import { config } from 'dotenv';
+import * as cookieParser from 'cookie-parser';
+import * as Session from 'express-session';
 
-config();
+const HOURS = 1000 * 60 * 60;
+config(); // dotenv activated.
 
-const mongodbUrl: string = process.env.MONGODB_URL as string;
+const mongodbUrl: string | undefined = process.env.MONGODB_URL;
 const app: express.Application = express();
 
 class BoardData {
@@ -37,24 +40,64 @@ if (mongodbUrl === undefined) {
     const db = client.db('blog');
     const boardCollections = db.collection('board');
     
+    if (typeof process.env.SECRET === 'string') {
+      app.use(Session.default({
+        secret: process.env.SECRET,
+        saveUninitialized: true,
+        cookie: {
+          maxAge: HOURS,
+        },
+        resave: false,
+      }));
+    } else {
+      console.log("Please make .env file and write SECRET Key");
+      return;
+    }
+    
     app.use(express.static(`${__dirname}/../public`));
     app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser.default());
     app.set('view engine', 'ejs');
     
+    let session: any;
+
     app.get('/', (req, res) => {
+      session = req.session;
       boardCollections.find().toArray()
       .then((result) => {
         res.render('index.ejs', {
           totalCount: result.length,
           data: result,
           user: {
-            role: undefined
+            role: session.userid,
           }
         });
       })
       .catch((error) => console.error(error));
     });
     
+    app.get('/login', (req, res) => {
+      res.render('login.ejs');
+    });
+
+    app.post('/login', (req, res) => {
+      if (req.body.id === process.env.ID && req.body.pw === process.env.PASSWORD) {
+        session = req.session;
+        session.userid = req.body.id;
+        console.log(session);
+        res.redirect('/');
+      }
+      else {
+        res.send('invalid id or pw');
+      }
+    });
+
+    app.get('/logout', (req, res) => {
+      req.session.destroy(() => req.session);
+      res.redirect('/');
+    })
+
     app.get('/board/:id', (req, res) => {
       let boardID: ObjectId;
       try {
@@ -91,6 +134,7 @@ if (mongodbUrl === undefined) {
         });
         
         app.get('/newpost', (req, res) => {
+          session = req.session;
           let boardID: ObjectId;
           try {
             boardID = new ObjectId(req.query.id as string);
@@ -103,17 +147,22 @@ if (mongodbUrl === undefined) {
             _id: boardID,
           })
           .then((result) => {
-            if (result === undefined) {
-              res.render('newpost.ejs', {
-                data: {
-                  title: '',
-                  body: '',
-                },
-              });
-            } else {
-              res.render('newpost.ejs', {
-                data: result,
-              });
+            if (session.userid === 'artemi') {
+              if (result === undefined) {
+                res.render('newpost.ejs', {
+                  data: {
+                    title: '',
+                    body: '',
+                  },
+                });
+              } else {
+                res.render('newpost.ejs', {
+                  data: result,
+                });
+              }
+            }
+            else {
+              res.redirect('/');
             }
           })
           .catch((error) => console.error(error));
@@ -151,7 +200,7 @@ if (mongodbUrl === undefined) {
           } catch (error) {
             res.status(404).send(`404 ERROR!! <br> ${error}`);
             return;
-      }
+          }
       
       const data = req.body;
       boardCollections.deleteOne({
