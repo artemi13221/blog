9caaa2b159049e599602b4bd058773f750c15245
artemi <artemi13221@gmail.com>
2021-08-24 13:18:39 +0900

    🔨 Add Error handling function

--- src/appp.ts
+++ src/appp.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import expressSession from 'express-session';
