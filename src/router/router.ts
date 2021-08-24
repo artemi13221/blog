92ce5e9b8db121683a56ee2db5738c02426747be
artemi <artemi13221@gmail.com>
2021-08-24 13:19:35 +0900

    ♻️ Refactoring: deprecated promise.

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import console from 'console';
+// import console from 'console';
 import { boardSchema } from '../db_schema';
 
 const router = express.Router();
@@ -9,27 +9,39 @@ const router = express.Router();
 
 // router.get('/login', );
 
-router.get('/', (req, res) => {
-  const page: number = parseInt(req.query.page as string) ?? 0;
+router.get('/', async (req, res, next) => {
+  const queryPage = req.query.page ?? '1';
+  if (typeof queryPage !== 'string') {
+    next();
+    return;
+  }
+  const page: number = parseInt(queryPage, 10) - 1;
+  if (page < 0) {
+    next();
+    return;
+  }
+
   const board = mongoose.model('Board', boardSchema);
-  board.find()
+  const findResult = await board.find()
     .select('_id title body')
     .skip(10 * page)
     .limit(10)
     .sort({ createAt: 'descending' })
-    .then(async (result) => {
-      res.render('index.ejs', {
-        totalCount: await board.find().select('_id').countDocuments(),
-        indexNum: result.length,
-        data: result,
-        user: {
-          role: undefined,
-        },
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    .exec();
+
+  if (!findResult) {
+    next();
+    return;
+  }
+
+  res.render('index.ejs', {
+    totalCount: await board.countDocuments(),
+    indexNum: findResult.length,
+    data: findResult,
+    user: {
+      role: undefined,
+    },
+  });
 });
 
 export default router;
