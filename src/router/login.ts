14f7ee57f8b37bcfaed45cbce995077af58bf51c
artemi <artemi13221@gmail.com>
2021-08-26 01:26:17 +0900

    🚧 WIP

--- src/router/login.ts
+++ src/router/login.ts
@@ -1,2 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+
+const router = express.Router();
+
+// GET Login Page
+router.get('/');
+
+// POST Login and logout
+router.post('/');
+
+// Module export
+export default router;
