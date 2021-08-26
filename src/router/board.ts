b8195ff24c7e61cc81ddac5fdc088ca667ac487a
artemi <artemi13221@gmail.com>
2021-08-26 19:28:28 +0900

    🔨 Add board router

--- src/router/board.ts
+++ src/router/board.ts
@@ -1,126 +1,117 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import { boardSchema } from '../db_schema';
+import { Types } from 'mongoose';
+import { BoardModel } from '../mongooseSchema';
 
 const router = express.Router();
-const Board = mongoose.model('Board', boardSchema);
+const findFailResultError = new Error('Mongoose Error: Cant find db by _id');
 
 // Write a board and create db
 router.post('/', async (req, res, next) => {
   const data = req.body;
-  const insertData = new Board({
+  const insertData = new BoardModel({
+    poster: 'test',
     title: data.title,
     body: data.body,
-    createAt: data.createAt,
-    modifiedAt: data.modifiedAt,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    isDeleted: false,
   });
   try {
     await insertData.save();
+    res.send('ok');
   } catch (error) {
     next(error);
   }
-  res.send('ok');
 });
 
 // Modify a board and update db
 router.put('/', async (req, res, next) => {
-  let boardID: mongoose.Types._ObjectId;
   const data = req.body;
-  if (data.id) {
-    next();
-    return;
-  }
-
+  const postId = data.id;
   try {
-    boardID = mongoose.Types.ObjectId(data.id);
-    await Board.updateOne({
-      _id: boardID,
+    await BoardModel.updateOne({
+      _id: Types.ObjectId(postId),
     }, {
       title: data.title,
       body: data.body,
-      modifiedAt: data.modifiedAt,
+      updatedAt: new Date(),
     });
+    res.send('ok');
   } catch (error) {
     next(error);
-    return;
   }
-
-  res.send('ok');
 });
 
 // Delete a board and delete db
 router.delete('/', async (req, res, next) => {
-  let boardID: mongoose.Types._ObjectId;
-  if (!req.body.id || typeof req.body.id !== 'string') {
-    next();
-    return;
-  }
+  const postId = req.body.id;
   try {
-    boardID = mongoose.Types.ObjectId(req.body.id);
-    if (await Board.findOne({ _id: boardID })) {
-      await Board.deleteOne({ _id: boardID });
-    } else {
-      throw new Error('undefined boardID');
-    }
+    await BoardModel.updateOne({
+      _id: Types.ObjectId(postId),
+    }, {
+      deletedAt: new Date(),
+      isDeleted: true,
+    });
+    res.send('ok');
   } catch (error) {
     next(error);
-    return;
   }
-
-  res.send('ok');
 });
 
 // Writer Page
 router.get('/newpost', async (req, res, next) => {
-  let boardID: mongoose.Types._ObjectId;
-  req.query.id = req.query.id ?? 'undefined';
+  const postId = req.query.id ?? 'undefined';
 
-  if (typeof req.query.id !== 'string') {
-    next();
-    return;
-  }
-  let findResult: mongoose.Callback;
-  if (req.query.id === 'undefined') {
+  try {
+    if (typeof postId !== 'string') {
+      throw new Error('Wrong parameter. Check your parameter');
+    }
+
+    if (postId !== 'undefined') {
+      const result = await BoardModel.findOne({
+        _id: Types.ObjectId(postId),
+      }).select('title body');
+
+      if (!result) {
+        throw findFailResultError;
+      }
+
+      res.render('newpost.ejs', {
+        data: result,
+      });
+      return;
+    }
+
     res.render('newpost.ejs', {
       data: {
         title: '',
         body: '',
       },
     });
-    return;
-  }
-
-  try {
-    boardID = mongoose.Types.ObjectId(req.query.id);
-    findResult = await Board.findOne({ _id: boardID }).select('title body');
   } catch (error) {
     next(error);
-    return;
   }
-  res.render('newpost.ejs', {
-    data: findResult,
-  });
 });
 
 // Read a board.
 router.get('/:id', async (req, res, next) => {
-  let boardID: mongoose.Types._ObjectId;
-  let findResult: mongoose.Callback;
+  const postId = req.params.id;
   try {
-    boardID = mongoose.Types.ObjectId(req.params.id);
-    findResult = await Board.findOne({ _id: boardID });
+    const result = await BoardModel.findOne({
+      _id: Types.ObjectId(postId),
+    });
+
+    if (!result) {
+      throw findFailResultError;
+    }
+
+    res.render('post.ejs', {
+      data: result,
+    });
   } catch (error) {
     next(error);
-    return;
   }
-  if (!findResult) {
-    next();
-    return;
-  }
-
-  res.render('post.ejs', {
-    data: findResult,
-  });
 });
 
-export default router;
+const boardRouter = router;
+export { boardRouter };
