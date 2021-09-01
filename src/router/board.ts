d7e58701b3e0200ec54b6611a611791973ee7a24
artemi <artemi13221@gmail.com>
2021-09-01 19:49:40 +0900

    🔨 Add Check session middleware

--- src/router/board.ts
+++ src/router/board.ts
@@ -5,59 +5,6 @@ import { BoardModel } from '../mongooseSchema';
 const router = express.Router();
 const findFailResultError = new Error('Mongoose Error: Cant find db by _id');
 
-// Write a board and create db
-router.post('/', async (req, res, next) => {
-  const data = req.body;
-  const insertData = new BoardModel({
-    poster: 'test',
-    title: data.title,
-    body: data.body,
-    createdAt: Date.now(),
-    updatedAt: Date.now(),
-    isDeleted: false,
-  });
-  try {
-    await insertData.save();
-    res.send('ok');
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Modify a board and update db
-router.put('/', async (req, res, next) => {
-  const data = req.body;
-  const postId = data.id;
-  try {
-    await BoardModel.updateOne({
-      _id: Types.ObjectId(postId),
-    }, {
-      title: data.title,
-      body: data.body,
-      updatedAt: new Date(),
-    });
-    res.send('ok');
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Delete a board and delete db
-router.delete('/', async (req, res, next) => {
-  const postId = req.body.id;
-  try {
-    await BoardModel.updateOne({
-      _id: Types.ObjectId(postId),
-    }, {
-      deletedAt: new Date(),
-      isDeleted: true,
-    });
-    res.send('ok');
-  } catch (error) {
-    next(error);
-  }
-});
-
 // Writer Page
 router.get('/newpost', async (req, res, next) => {
   const postId = req.query.id ?? 'undefined';
@@ -112,5 +59,68 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
+// Check Session role
+router.use((req, res, next) => {
+  if ((req.session as any).role !== 'admin') {
+    res.send('role-error');
+    return;
+  }
+  next();
+});
+
+// Write a board and create db
+router.post('/', async (req, res, next) => {
+  const data = req.body;
+  const insertData = new BoardModel({
+    poster: 'test',
+    title: data.title,
+    body: data.body,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isDeleted: false,
+  });
+  try {
+    await insertData.save();
+    res.send('ok');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Modify a board and update db
+router.put('/', async (req, res, next) => {
+  const data = req.body;
+  const postId = data.id;
+  try {
+    await BoardModel.updateOne({
+      _id: Types.ObjectId(postId),
+    }, {
+      title: data.title,
+      body: data.body,
+      updatedAt: new Date(),
+    });
+    res.send('ok');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete a board and delete db
+router.delete('/', async (req, res, next) => {
+  const postId = req.body.id;
+  try {
+    await BoardModel.updateOne({
+      _id: Types.ObjectId(postId),
+    }, {
+      deletedAt: new Date(),
+      isDeleted: true,
+    });
+    res.send('ok');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// export module
 const boardRouter = router;
 export { boardRouter };
